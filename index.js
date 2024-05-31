6f84e203478e659298a586a69bcb35552eb03460
gelpilar <pilarscollo@gmail.com>
2024-05-31 15:47:06 -0300

    tercer arreglo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const jsonServer = require("json-server")
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port= process.eventNames.PORT || 4000
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
